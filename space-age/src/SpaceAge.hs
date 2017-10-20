50aa0284adbc68d6a17f2dca66ae875da9b03e03
Jack Hsu <jack.hsu@gmail.com>
2017-10-20 19:06:43 -0400

    init

---
+++ space-age/src/SpaceAge.hs
@@ -0,0 +1,23 @@
+module SpaceAge (Planet(..), ageOn) where
+
+data Planet =
+  | Mercury
+  | Venus
+  | Earth
+  | Mars
+  | Jupiter
+  | Saturn
+  | Uranus
+  | Neptune
+
+ageOn :: Planet -> Float -> Float
+ageOn planet seconds = case planet of
+  Mercury -> earthYear / 0.2408467
+  Venus -> earthYear / 0.61519726
+  Mars -> earthYear / 1.8808158
+  Jupiter -> earthYear /11.862615
+  Saturn -> earthYear / 29.447498
+  Uranus -> earthYear / 84.016846
+  Neptune -> earthYear / 164.79132
+  _ -> earthYear
+  where earthYear = seconds / 31557600
